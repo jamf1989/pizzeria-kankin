5b3d78b3d72e741f365e100fa71619e49b917308
Jose Angel <jamf1989@hotmail.com>
2021-10-14 20:00:28 -0500

    se agrega archivo css y js para las animaciones de las pizzas

---
+++ index.js
@@ -0,0 +1,13 @@
+let animado = document.querySelectorAll(".animate");
+
+function mostrarScroll() {
+    let scrollTop = document.documentElement.scrollTop;
+    for (let i=0; i < animado.length; i++) {
+        let alturaAnimado = animado[i].offsetTop;
+        if (alturaAnimado < scrollTop) {
+            animado[i].style.opacity = 1;
+        }
+    }
+}
+
+window.addEventListener('scroll', mostrarScroll);
