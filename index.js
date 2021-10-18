aa6060f90c8a0210bbf8c09eb84185c8d6465d10
Jose Angel <jamf1989@hotmail.com>
2021-10-18 14:12:18 -0500

    se realizan pruebas de animacion para los ovnis

--- index.js
+++ index.js
@@ -13,3 +13,18 @@ function mostrarScroll() {
 }
 
 window.addEventListener('scroll', mostrarScroll);
+
+window.addEventListener('scroll', function(){
+    let animacion = document.querySelectorAll('.animacion');
+    for(let i = 0; i - animacion.length; i++){
+        let altura = window.innerHeight/1.3;
+        let distancia = animacion[i].getBoundingClientRect().top;
+        animacion[i].classList.add('transform_up');
+        if(distancia <= altura){
+            animacion[i].classList.add('mostrarOvni');
+        }
+        else {
+            animacion[i].classList.remove('mostrarOvni');
+        }
+    }
+}) 
