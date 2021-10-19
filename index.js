8b2757c27741cd0bd04f6a8e5c5478be8844705a
Jose Angel <jamf1989@hotmail.com>
2021-10-19 10:57:34 -0500

    se agrga animaciones a las imagenes de las pizzas y a los ovnis

--- index.js
+++ index.js
@@ -1,30 +1,47 @@
-let animado = document.querySelectorAll(".animate");
+//let animado = document.querySelectorAll(".animate");
 
-function mostrarScroll() {
-    let scrollTop = document.documentElement.scrollTop;
-    for (let i=0; i < animado.length; i++) {
-        let alturaAnimado = animado[i].offsetTop;
-        if (alturaAnimado < scrollTop) {
-            animado[i].style.opacity = 1;
-            animado[i].classList.add("mostrar");
+//function mostrarScroll() {
+//    let scrollTop = document.documentElement.scrollTop;
+//    for (let i=0; i < animado.length; i++) {
+//        let alturaAnimado = animado[i].offsetTop;
+//        if (alturaAnimado < scrollTop) {
+//            animado[i].style.opacity = 1;
+//            animado[i].classList.add("mostrar");
+//
+//        }
+//    }
+//}
 
-        }
-    }
-}
+//window.addEventListener('scroll', mostrarScroll);
 
-window.addEventListener('scroll', mostrarScroll);
+//window.addEventListener('scroll', function(){
+//    let animacion = document.querySelectorAll('.animacion');
+//    for(let i = 0; i - animacion.length; i++){
+//        let altura = window.innerHeight/1.3;
+//        let distancia = animacion[i].getBoundingClientRect().top;
+//        animacion[i].classList.add('transform_up');
+//        if(distancia <= altura){
+//            animacion[i].classList.add('mostrarOvni');
+//        }
+//        else {
+//            animacion[i].classList.remove('mostrarOvni');
+//        }
+//    }
+//}) 
 
 window.addEventListener('scroll', function(){
-    let animacion = document.querySelectorAll('.animacion');
-    for(let i = 0; i - animacion.length; i++){
+    
+    let elemento = document.querySelectorAll('.animacion');
+    
+    for(i = 0; i < elemento.length; i++){
         let altura = window.innerHeight/1.3;
-        let distancia = animacion[i].getBoundingClientRect().top;
-        animacion[i].classList.add('transform_up');
+        let distancia = elemento[i].getBoundingClientRect().top;
+        console.log(distancia, altura);
+        elemento[i].classList.add('transform_up');
         if(distancia <= altura){
-            animacion[i].classList.add('mostrarOvni');
-        }
-        else {
-            animacion[i].classList.remove('mostrarOvni');
+            elemento[i].classList.add('mostrar');
+        } else {
+            elemento[i].classList.remove('mostrar')
         }
     }
-}) 
+})
